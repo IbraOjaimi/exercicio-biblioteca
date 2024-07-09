0d94fe76f442be2b4624587c03ea02fe8586e758
IbraOjaimi <ibrahim.ojaimi@hotmail.com>
2024-07-08 22:23:05 -0300

    funcionando

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const prompt = require('prompt-sync')({sigint: true});
-const {array, adicionar, listar, editar, excluir, sair} = require('./modulo.js')
 
-array
+const {adicionar, listar, editar, excluir} = require('./modulo.js')
 
 while (true) {
     console.log(`
@@ -14,10 +13,23 @@ while (true) {
 
     let opcao = prompt('Escolha uma opção: ');
 
-    adicionar
-    listar
-    editar
-    excluir
-    sair
-
+    switch (opcao) {
+        case '1':
+            adicionar();
+            break;
+        case '2':
+            listar();
+            break;
+        case '3':
+            editar();
+            break;
+        case '4':
+            excluir();
+            break;
+        case '0':
+            console.log('Saindo do sistema. Até logo!');
+            process.exit();
+        default:
+            console.log('Opção inválida, digite uma opção válida!');
+    }
 }
--- modulo.js
+++ modulo.js
@@ -1,179 +1,107 @@
-let array = function(){
-    let bilioteca = [];
-    let genero = [];
-}
+const prompt = require('prompt-sync')({ sigint: true });
 
-let adicionar = function(){
+let biblioteca = [];
 
-    if(opcao === '1') {
-        let nome = prompt('Nome do livro: ');
-        let autor = prompt('Autor do livro: ');
-        let quantidade = prompt('Tamanho do livro (quant. pág.): ');
-            console.log(`Escolha o gênero: 
+function adicionar() {
+    let nome = prompt('Nome do livro: ');
+    let autor = prompt('Autor do livro: ');
+    let quantidade = prompt('Tamanho do livro (quant. pág.): ');
 
-            1. Romance
-            2. Aventura
-            3. Drama
-            4. Crônica
-            5. Conto
-            6. Poema
-            `);
-            genero = prompt('Escolha uma opção: ')
+    console.log(`Escolha o gênero: 
+    1. Romance
+    2. Aventura
+    3. Drama
+    4. Crônica
+    5. Conto
+    6. Poema`);
 
-            if(genero === '1'){
-                genero = 'Romance'
-                biblioteca.push({ nome, autor, quantidade, genero });
-                console.log('Livro adicionado com sucesso!', biblioteca);
-            } else if (genero === '2'){
-                genero = 'Aventura'
-                biblioteca.push({ nome, autor, quantidade, genero });
-                console.log('Livro adicionado com sucesso!', biblioteca);
-            } else if (genero === '3'){
-                genero = 'Drama'
-                biblioteca.push({ nome, autor, quantidade, genero });
-                console.log('Livro adicionado com sucesso!', biblioteca);
-            } else if (genero === '4'){
-                genero = 'Crônica'
-                biblioteca.push({ nome, autor, quantidade, genero });
-                console.log('Livro adicionado com sucesso!', biblioteca);
-            } else if (genero === '5'){
-                genero = 'Conto'
-                biblioteca.push({ nome, autor, quantidade, genero });
-                console.log('Livro adicionado com sucesso!', biblioteca);
-            } else if (genero === '6'){
-                genero = 'Poema'
-                biblioteca.push({ nome, autor, quantidade, genero });
-                console.log('Livro adicionado com sucesso!', biblioteca);
-            }
-    }
-}
+    let generoOpcao = prompt('Escolha uma opção: ');
+    let genero = ["Romance", "Aventura", "Drama", "Crônica", "Conto", "Poema"][generoOpcao - 1];
 
-let listar = function(){
-    if (opcao === '2') {
-        if (biblioteca.length === 0) {
-            console.log('Nenhuma livro cadastrado.');
-        } else {
-            biblioteca.forEach((biblioteca, index) => {
-                console.log(
-                    `${index + 1}. Livro: ${biblioteca.nome} - Autor: ${biblioteca.autor} - Tamanho: ${biblioteca.quantidade} pág. - Gênero: ${biblioteca.genero}`
-                );
-            });
-        }
-    } else if (opcao === '0') {
-        console.log('Saindo do sistema. Até logo!');
-        process.exit();
-    }
-}
-
-let editar = function(){
-    if (opcao === '3') {
-        if (biblioteca.lenght === 0) {
-            console.log('Nenhum livro cadastrado.');
-        } else {
-        biblioteca.forEach((biblioteca, index) => {
-            console.log(
-                `${index + 1}. Livro: ${biblioteca.nome} - Autor: ${biblioteca.autor} - Tamanho: ${biblioteca.quantidade} - Gênero: ${biblioteca.genero}`
-            );
-        });
-        let editar = prompt('Qual livro deseja editar? ');
-        index = editar-1;
-        let dados = prompt('Você deseja editar algum dado? ');
-            if(dados == 'sim'){
-            console.log(`
-
-            Qual dado você deseja editar?
-            1 - Livro.
-            2 - Autor.
-            3 - Tamanho do livro (quant. pág.).
-            4 - Gênero.
-            0 - Sair.
-
-            `);
-
-            let edit_dados = prompt('Opção: ');
-
-            if(edit_dados === '1') {
-                let novo_nome = prompt('Qual é o nome do livro? ');
-                biblioteca[index].nome = novo_nome;
-            } else if (edit_dados === '2') {
-                let novo_autor = prompt('Qual é o nome do autor? ');
-                biblioteca[index].autor = novo_autor;
-            } else if (edit_dados === '3') {
-                let novo_tamanho = prompt('Qual é o tamanho (quant. pág.)? ')
-                biblioteca[index].quantidade = novo_tamanho;
-            } else if (edit_dados === '4') {
-                console.log(`Escolha o gênero: 
-
-                    1. Romance
-                    2. Aventura
-                    3. Drama
-                    4. Crônica
-                    5. Conto
-                    6. Poema
-                    `);
-                    novo_genero = prompt('Escolha uma opção: ')
-
-                if(novo_genero === '1'){
-                    novo_genero = 'Romance';
-                } else if (novo_genero === '2'){
-                    novo_genero = 'Aventura';
-                } else if (novo_genero === '3'){
-                    novo_genero = 'Drama';
-                } else if (novo_genero === '4'){
-                    novo_genero = 'Crônica';
-                } else if (novo_genero === '5'){
-                    novo_genero = 'Conto';
-                } else if (novo_genero === '6'){
-                    novo_genero = 'Poema';
-                }
-                biblioteca[index].genero = novo_genero;
-
-            } else if ( edit_dados === '0'){
-                console.log('Até logo!')
-                process.exit();
-            }
-            }else if (dados == 'nao') {
-                console.log('Até logo!');
-                process.exit();
-            }
-        }
-    }
-}
-
-let excluir = function(){
-    if (opcao === '4') {
-        if (biblioteca.length === 0) {
-            console.log('Nenhuma livro cadastrado.');;
-        } else {
-            biblioteca.forEach((biblioteca, index) => {
-                console.log(
-                    `${index + 1}. Livro: ${biblioteca.nome} - Autor: ${biblioteca.autor} - Tamanho: ${biblioteca.genero}`
-                );
-            });
-        } 
-        
-        let cancelar = prompt('Qual livro deseja excluir? ');
-        if(cancelar === '0'){
-            console.log('\n Não livro n.0');
-        } else if (cancelar > biblioteca.length){
-            console.log('\n Digite um livro valido!');
-        } else {
-        index = cancelar - 1;
-        biblioteca.splice(index, 1);
-        console.log('\n Entrada do livro Cancelado!');
-        } 
-    }
-}
-
-let sair = function(){
-    if (opcao === '0') {
-        console.log('Até logo!');
-        process.exit();
+    if (genero) {
+        biblioteca.push({ nome, autor, quantidade, genero });
+        console.log('Livro adicionado com sucesso!', biblioteca);
     } else {
-        console.log('Opção invalida, digite uma opção valida!');
+        console.log('Gênero inválido!');
+    }
+}
+
+function listar() {
+    if (biblioteca.length === 0) {
+        console.log('Nenhum livro cadastrado.');
+    } else {
+        biblioteca.forEach((livro, index) => {
+            console.log(`${index + 1}. Livro: ${livro.nome} - Autor: ${livro.autor} - Tamanho: ${livro.quantidade} pág. - Gênero: ${livro.genero}`);
+        });
+    }
+}
+
+function editar() {
+    listar();
+    let editar = parseInt(prompt('Qual livro deseja editar? ')) - 1;
+
+    if (editar >= 0 && editar < biblioteca.length) {
+        let livro = biblioteca[editar];
+
+        console.log(`
+        Qual dado você deseja editar?
+        1 - Nome.
+        2 - Autor.
+        3 - Tamanho (quant. pág.).
+        4 - Gênero.
+        0 - Sair.`);
+
+        let edit_dados = prompt('Opção: ');
+
+        switch (edit_dados) {
+            case '1':
+                livro.nome = prompt('Qual é o novo nome do livro? ');
+                break;
+            case '2':
+                livro.autor = prompt('Qual é o novo nome do autor? ');
+                break;
+            case '3':
+                livro.quantidade = prompt('Qual é o novo tamanho (quant. pág.)? ');
+                break;
+            case '4':
+                console.log(`Escolha o novo gênero: 
+                1. Romance
+                2. Aventura
+                3. Drama
+                4. Crônica
+                5. Conto
+                6. Poema`);
+
+                let novo_generoOpcao = prompt('Escolha uma opção: ');
+                livro.genero = ["Romance", "Aventura", "Drama", "Crônica", "Conto", "Poema"][novo_generoOpcao - 1];
+                break;
+            case '0':
+                console.log('Saindo...');
+                return;
+            default:
+                console.log('Opção inválida!');
+        }
+        console.log('Livro atualizado com sucesso!', livro);
+    } else {
+        console.log('Livro não encontrado!');
+    }
+}
+
+function excluir() {
+    listar();
+    let cancelar = parseInt(prompt('Qual livro deseja excluir? ')) - 1;
+
+    if (cancelar >= 0 && cancelar < biblioteca.length) {
+        biblioteca.splice(cancelar, 1);
+        console.log('Livro excluído com sucesso!');
+    } else {
+        console.log('Livro não encontrado!');
     }
 }
 
 module.exports = {
-array, adicionar, listar, editar, excluir, sair
-}
+    adicionar,
+    listar,
+    editar,
+    excluir
+};
